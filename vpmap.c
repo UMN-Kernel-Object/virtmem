9df77053db3a4c6b4a049314aefd3ad03f6dd714
Chris Kauffman <kauffman77@gmail.com>
2023-03-31 19:14:56 -0500

    First pass at formatted output for physical pages.
    In reasonable working order though could use some docs and
    cleanup. Struct layout with bitfields seems to be working fine. Worked
    out kinks in pread() and output is compact but useful.

--- vpmap.c
+++ vpmap.c
@@ -1,6 +1,11 @@
 // vpmap.c: initial checkin
 
 #include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
 
 // The following two structs use bitfields to enable easier parsing of
 // the data provided in /proc/<pid>/maps that will be a 64-bit number
@@ -18,7 +23,7 @@ typedef struct {
   uint32_t exclu   :  1;    // Bit 56 page exclusively mapped (since 4.2)                               
   uint32_t wprot   :  1;    // Bit 57 pte is uffd-wp write-protected (since 5.13) (see Userfaultfd)     
   uint32_t zeros   :  3;    // Bits 58-60 zero                                                          
-  uint32_t isanon  :  1;    // Bit 61 page is file-page or shared-anon (since 3.5)                      
+  uint32_t isfile  :  1;    // Bit 61 page is file-page or shared-anon (since 3.5)                      
   uint32_t swapped :  1;    // Bit 62 page swapped                                                      
   uint32_t present :  1;    // Bit 63 page present                                                      
 } entry_present_t;
@@ -26,12 +31,12 @@ typedef struct {
 // map entry which is swapped out
 typedef struct {
   uint32_t swaptyp :  5;    // Bits 0-4 swap type if swapped
-  uint64_t pfn     : 50;    // Bits 5-54 swap offset if swapped
+  uint64_t swapoff : 50;    // Bits 5-54 swap offset if swapped
   uint32_t dirty   :  1;    // Bit 55 pte is soft-dirty (see Soft-Dirty PTEs)                           
   uint32_t exclu   :  1;    // Bit 56 page exclusively mapped (since 4.2)                               
   uint32_t wprot   :  1;    // Bit 57 pte is uffd-wp write-protected (since 5.13) (see Userfaultfd)     
   uint32_t zeros   :  3;    // Bits 58-60 zero                                                          
-  uint32_t isanon  :  1;    // Bit 61 page is file-page or shared-anon (since 3.5)                      
+  uint32_t isfile  :  1;    // Bit 61 page is file-page or shared-anon (since 3.5)                      
   uint32_t swapped :  1;    // Bit 62 page swapped                                                      
   uint32_t present :  1;    // Bit 63 page present                                                      
 } entry_swapped_t;
@@ -42,8 +47,129 @@ typedef struct {
 typedef union {
   entry_present_t present;
   entry_swapped_t swapped;
+  uint64_t bytes;
 } map_entry_t;
 
+#define BUFSIZE 2048
+
+void print_map_entry(map_entry_t m, char *prefix){
+  printf("%s",prefix);
+  // printf("bytes: %016lx  ",m.bytes);
+  // printf("present: %d  ", m.present.present);
+  // printf("swapped: %d  ", m.present.swapped);
+  if(m.present.present){
+    printf("present  ");
+    printf("pfn: %lx  ",m.present.pfn);
+  }
+  else if(m.present.swapped){
+    printf("swapped  ");
+    printf("swaptyp: %lx  ",m.swapped.swaptyp);
+    printf("swapoff: %lx  ",m.swapped.swapoff);
+  }
+  if(m.present.present || m.present.swapped){
+    printf("dirty: %d  ",m.present.dirty);
+    printf("exclu: %d  ",m.present.exclu);
+    printf("wprot: %d  ",m.present.wprot);
+    printf("isfile: %d  ",m.present.isfile);
+  }
+  printf("\n");
+}
+
 int main(int argc, char *argv[]){
+
+  if(argc < 2){
+    printf("usage: sudo %s <PID>\n",argv[0]);
+    return 0;
+  }
+
+  if(geteuid() != 0){
+    printf("This program only makes sense to run as root / sudo\n");
+    printf("Normal user runs will produce little meaningful data\n");
+    return 0;
+  }
+
+  char errmsg[BUFSIZE];
+
+  pid_t pid = atol(argv[1]);
+  printf("Process %d\n",pid);
+
+  uint64_t pagesize = sysconf(_SC_PAGE_SIZE);
+
+  char maps_fname[BUFSIZE];
+  snprintf(maps_fname,BUFSIZE,"/proc/%d/maps",pid);
+
+  FILE *maps_file = fopen(maps_fname,"r");
+  if(maps_file == NULL){
+    snprintf(errmsg,BUFSIZE,"Error opening '%s'",maps_fname);
+    perror(errmsg);
+    exit(EXIT_FAILURE);
+  }
+
+  char pagemap_fname[BUFSIZE];
+  snprintf(pagemap_fname,BUFSIZE,"/proc/%d/pagemap",pid);
+
+  int pagemap_fd = open(pagemap_fname, O_RDONLY);
+  if(pagemap_fd == -1){
+    snprintf(errmsg,BUFSIZE,"Error opening '%s'",pagemap_fname);
+    perror(errmsg);
+    exit(EXIT_FAILURE);
+  }
+    
+  while(!feof(maps_file)){
+    char line[BUFSIZE];
+    char *result = fgets(line, BUFSIZE, maps_file);
+    if(result == NULL){
+      perror("Error reading line\n");
+      exit(EXIT_FAILURE);
+    }
+    line[strlen(line)-1] = '\0'; // chop the \n
+
+    off_t start_addr, stop_addr;
+    int count = sscanf(line, "%lx-%lx", &start_addr, &stop_addr);
+    if(count < 2){
+      printf("Error  parsing start/stop addresses in '%s'\n",line);
+      exit(EXIT_FAILURE);
+    }
+
+    printf("%s\n",line);
+    for(off_t cur_addr = start_addr; cur_addr < stop_addr; cur_addr += pagesize){
+
+      off_t virt_pn = cur_addr / pagesize;
+      off_t offset =  virt_pn * sizeof(map_entry_t);
+
+      map_entry_t entry;
+    
+      ssize_t ret = pread(pagemap_fd, &entry, sizeof(map_entry_t), offset);
+      if(ret != sizeof(map_entry_t)){
+        printf("requested %ld from fd %d, read %ld bytes\n",
+               sizeof(map_entry_t),pagemap_fd,ret);
+        perror("Failed to read enough bytes");
+        exit(EXIT_FAILURE);
+      }
+
+      // off_t ret = lseek(pagemap_fd, offset, SEEK_SET);
+      // if(ret != offset){
+      //   printf("requested seek to offset %lx, returned %ld\n",
+      //          offset,ret);
+      //   perror("Failed to seek");
+      //   exit(EXIT_FAILURE);
+      // }
+    
+      // ssize_t nread = read(pagemap_fd, &entry, sizeof(map_entry_t));
+      // if(nread != sizeof(map_entry_t)){
+      //   printf("requested %ld from fd %d, read %ld bytes\n",
+      //          sizeof(map_entry_t),pagemap_fd,nread);
+      //   perror("Failed to read enough bytes");
+      //   exit(EXIT_FAILURE);
+      // }
+
+      // printf("+ vaddr: %lx   pagemap off: %lx  entry.bytes: %lx\n",
+      //        cur_addr, offset, entry.bytes);
+      printf("| vpn: %lx  ",virt_pn);
+      print_map_entry(entry,"");
+    }
+    printf("\n");
+  }
+
   return 0;
 }
