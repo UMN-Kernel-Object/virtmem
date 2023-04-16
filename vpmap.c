31e43e06d56d6515a80f0c6bb15ad680ea839c74
Janani Kannan <kanna128@umn.edu>
2023-04-16 17:09:53 -0500

    code cleanup + comments
    Signed-off-by: Janani Kannan <kanna128@umn.edu>

--- vpmap.c
+++ vpmap.c
@@ -1,5 +1,8 @@
 // vpmap.c: initial checkin
 
+// Program that attempts to translate virtual 
+// addresses to physical addresses in a Linux system 
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
@@ -9,7 +12,7 @@
 
 // The following two structs use bitfields to enable easier parsing of
 // the data provided in /proc/<pid>/maps that will be a 64-bit number
-// with bits representing a aspects of the page. The bit layouts are
+// with bits representing various aspects of the page. The bit layouts are
 // extracted from the kernel docs on the /proc interface to page
 // tables which is here:
 // https://docs.kernel.org/admin-guide/mm/pagemap.html
@@ -43,10 +46,10 @@ typedef struct {
 
 // union of the two struct types so that one can access via
 // map_entry_t entry;
-// entry.present.pfn == ???;
+// entry.existing_entry.pfn == <known pfn>;
 typedef union {
-  entry_present_t present;
-  entry_swapped_t swapped;
+  entry_present_t existing_entry;
+  entry_swapped_t swapped_entry;
   uint64_t bytes;
 } map_entry_t;
 
@@ -57,20 +60,21 @@ void print_map_entry(map_entry_t m, char *prefix){
   // printf("bytes: %016lx  ",m.bytes);
   // printf("present: %d  ", m.present.present);
   // printf("swapped: %d  ", m.present.swapped);
-  if(m.present.present){
+
+  if(m.existing_entry.present){ // if entry is present, print its page frame number in memory
     printf("present  ");
-    printf("pfn: %lx  ",m.present.pfn);
+    printf("pfn: %lx  ",m.existing_entry.pfn);
   }
-  else if(m.present.swapped){
+  else if(m.existing_entry.swapped){ // otherwise, if entry was swapped, print information about the swap
     printf("swapped  ");
-    printf("swaptyp: %lx  ",m.swapped.swaptyp);
-    printf("swapoff: %lx  ",m.swapped.swapoff);
+    printf("swaptyp: %lx  ",m.swapped_entry.swaptyp);
+    printf("swapoff: %lx  ",m.swapped_entry.swapoff);
   }
-  if(m.present.present || m.present.swapped){
-    printf("dirty: %d  ",m.present.dirty);
-    printf("exclu: %d  ",m.present.exclu);
-    printf("wprot: %d  ",m.present.wprot);
-    printf("isfile: %d  ",m.present.isfile);
+  if(m.existing_entry.present || m.existing_entry.swapped){
+    printf("dirty: %d  ",m.existing_entry.dirty);
+    printf("exclu: %d  ",m.existing_entry.exclu);
+    printf("wprot: %d  ",m.existing_entry.wprot);
+    printf("isfile: %d  ",m.existing_entry.isfile);
   }
   printf("\n");
 }
@@ -90,6 +94,7 @@ int main(int argc, char *argv[]){
 
   char errmsg[BUFSIZE];
 
+  // pid of process to fetch information about
   pid_t pid = atol(argv[1]);
   printf("Process %d\n",pid);
 
@@ -112,14 +117,17 @@ int main(int argc, char *argv[]){
   if(pagemap_fd == -1){
     snprintf(errmsg,BUFSIZE,"Error opening '%s'",pagemap_fname);
     perror(errmsg);
+    fclose(maps_file);
     exit(EXIT_FAILURE);
   }
     
-  while(!feof(maps_file)){
+  while(!feof(maps_file)){ // reading each line of the maps_file
     char line[BUFSIZE];
     char *result = fgets(line, BUFSIZE, maps_file);
     if(result == NULL){
       perror("Error reading line\n");
+      fclose(maps_file);
+      close(pagemap_fd);
       exit(EXIT_FAILURE);
     }
     line[strlen(line)-1] = '\0'; // chop the \n
@@ -128,10 +136,16 @@ int main(int argc, char *argv[]){
     int count = sscanf(line, "%lx-%lx", &start_addr, &stop_addr);
     if(count < 2){
       printf("Error  parsing start/stop addresses in '%s'\n",line);
+      fclose(maps_file);
+      close(pagemap_fd);
       exit(EXIT_FAILURE);
     }
 
     printf("%s\n",line);
+
+    // address conversion for a single mapping in maps_file
+    // each mapping exists in the space defined
+    // by this range: start_addr to stop_addr
     for(off_t cur_addr = start_addr; cur_addr < stop_addr; cur_addr += pagesize){
 
       off_t virt_pn = cur_addr / pagesize;
